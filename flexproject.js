f62c524a6eb2a5d8da64814e310cc483033fabb0
amlak befekadu <amlakgirma@yahoo.com>
2017-04-06 13:32:59 -0400

    All works well

--- flexproject.js
+++ flexproject.js
@@ -8,38 +8,50 @@ $(document).ready(function(){
   var count=1;
   function moveDisk() {
 
-       let smallestDisk = $(sourceContainer).children().eq(0)
+    let smallestSourceDisk = $(sourceContainer).children().eq(0)
+    let smallestTargetDisk = $(targetContainer).children().eq(0)
+    //console.log($(smallestSourceDisk).attr("id")
+    console.log(smallestSourceDisk.attr('id'), smallestTargetDisk.attr('id'))
 
-        smallestDisk.remove()
-       $(targetContainer).prepend(smallestDisk)
-       containerIsSelected = false;
-        targetContainer = this;
+    if(parseInt(smallestSourceDisk.attr('id')) < parseInt(smallestTargetDisk.attr('id')) || smallestTargetDisk.attr('id') == undefined) {
+      smallestSourceDisk.remove()
+      $(targetContainer).prepend(smallestSourceDisk)
+    }else{
+      alert("Wrong move")
+    }
+
+    containerIsSelected = false;
 
 
-     $("span").text(count++);if(count==8){
-     $("span").text("Game Over");
-     }
+
+    $("span").text(count++);if(count==8){
+      $("span").text("Game Over");
+
+    }
   };
 
   $('.table').on('click', function() {
-           if(containerIsSelected == false) {
-             sourceContainer = this
-                   if($(this).children().length <1){
-                      //check if there is a child in the container
-                     alert("You Click Wrong Contianer");
-                    } else{
-                       containerIsSelected = true
-                     }
-             }else {
-                     targetContainer = this
-                      moveDisk()
+    if(containerIsSelected == false) {
+      sourceContainer = this
+      if($(this).children().length <1){
+        //check if there is a child in the container
+        alert("You Click Empty Contianer");
+      } else{
+        containerIsSelected = true
+      }
+    }else
+    {
+      targetContainer = this
 
-                    }
+
+      moveDisk()
+
+    }
   });
-        $("h2").on("click",function(){
-                    $(".rule").slideDown(5000);
-                  $("h2").click(function(){
-                    $(".rule").slideUp(2000);
-                     });
-            });
+  $("h2").on("click",function(){
+    $(".rule").slideDown(5000);
+    $("h2").click(function(){
+      $(".rule").slideUp(2000);
+    });
+  });
 });
