0ffc015f5c20d9ccf00ed226dcc36258cdb628dd
Andrew Whitley <awhitley1233@gmail.com>
2017-04-21 11:15:08 -0400

    added inline code comments / feedback.md

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# project-one
-I found this project interesting and challenging. Comfort level 3
-competeness 3
+# Towers of Hanoi
+*by Amlak Girma*
+
+## Description
+put a brief description of the project here
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- jQuery
+
+## User Stories
+- List user stories here in bullet format
+
+## Approach Taken / Unsolved Problems
+put a brief summary here on how you planned out the app / any next steps you would like to add to it
--- flexproject.js
+++ flexproject.js
@@ -1,57 +1,53 @@
 $(document).ready(function(){
+  // Nice use of a $(document).ready()
 
   var containerIsSelected = false
   var sourceContainer = null
   var targetContainer = null
-  var count=1;
+  var count = 1;
+  // regarding semicolons, try to stay consistent. Either use them everywhere they
+  // should be placed, or don't use them at all (the browser will insert them for you)
+
   function moveDisk() {
     let smallestDisk = $(sourceContainer).children().eq(0)
-
-
     smallestDisk.remove()
-
-
-
-  $(targetContainer).prepend(smallestDisk)
-    containerIsSelected=false;
-    targetContainer= this;
-
-     $("span").text(count++);if(count==8){
-     $("span").text("Game Over");
-     }
+    $(targetContainer).prepend(smallestDisk)
+    containerIsSelected = false;
+    // You don't need to reassign targetContainer here, already did so in the $('table').on('click')
+    $("span").text(count++);
+    if(count == 8){
+      // Not sure that you want to end the game after only 8 moves
+      $("span").text("Game Over");
+    }
   };
+
   $('.table').on('click', function() {
-
     if(containerIsSelected == false) {
-
       sourceContainer = this
-      if($(sourceContainer).children().length <1){
-    //check if there is a child in the container
-         alert("You Click Wrong Contianer");
-       } else{
+      if($(sourceContainer).children().length < 1){
+        //check if there is a child in the container
+        alert("You Click Wrong Contianer");
+      } else {
        containerIsSelected = true
-     }
-    //===================================================//
-    }
-
-       else {
-
+      }
+    } else {
       targetContainer = this
-
       moveDisk()
-
+      // Nice abstraction of the moveDisk functionality
     }
-
   })
 
 
 
-$("h2").on("click",function(){
-  $(".rule").slideDown(5000);
-  $("h2").click(function(){
-          $(".rule").slideUp(2000);
-      });
+  $("h2").on("click",function(){
+    $(".rule").slideDown(5000);
+    $("h2").click(function(){
+      $(".rule").slideUp(2000);
+    });
+  });
+  // Nice use of jQuery animations but maybe make the div to click to trigger this
+  // animation more apparent (like a label that says 'Click Me')
 });
 
-
-});
+// Good code structure of all variables declared at the top, functions in the middle,
+// and event listeners at the bottom ++
